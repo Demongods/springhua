97844c2678f5aea35106576000420f6b02e07cbf
user <318473508@qq.com>
2020-04-30 16:16:17 +0800

    第二次尝试

--- src/main/java/com/kai/hh/springhua/test/testdemo.java
+++ src/main/java/com/kai/hh/springhua/test/testdemo.java
@@ -1,5 +1,9 @@
 package com.kai.hh.springhua.test;
 
 public class testdemo {
-
+    public static void main(String[] args){
+        int a=1111;
+        Integer b=1111;
+        System.out.println(a==b);
+    }
 }
